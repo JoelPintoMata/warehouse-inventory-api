c27b564908caf4365616556549c17628f3e2da5c
JoelPintoMata <joelmatacv@runbox.com>
2020-11-02 01:22:26 +0100

    Update ProductServiceImplTest.java

--- src/test/java/com/datawarehouse/product/service/ProductServiceImplTest.java
+++ src/test/java/com/datawarehouse/product/service/ProductServiceImplTest.java
@@ -106,50 +106,14 @@ public class ProductServiceImplTest {
 
     @Test
     public void givenProductWithUnavailableArticle_whenRequestAvailability_thenOk() {
-        StockEntity stockEntity1 = new StockEntity();
-        stockEntity1.setStock(20L);
-        ArticleEntity articleEntity1 = new ArticleEntity();
-        articleEntity1.setId(1L);
-        articleEntity1.setArticleId(1L);
-        articleEntity1.setStock(stockEntity1);
-        when(articleRepository.findByArticleId(articleEntity1.getId())).thenReturn(Optional.of(articleEntity1));
-
-        StockEntity stockEntity2 = new StockEntity();
-        stockEntity2.setStock(1L);
-        ArticleEntity articleEntity2 = new ArticleEntity();
-        articleEntity2.setId(2L);
-        articleEntity2.setArticleId(2L);
-        articleEntity2.setStock(stockEntity2);
-        when(articleRepository.findByArticleId(articleEntity2.getId())).thenReturn(Optional.of(articleEntity2));
-
-        StockEntity stockEntity3 = new StockEntity();
-        stockEntity3.setStock(30L);
-        ArticleEntity articleEntity3 = new ArticleEntity();
-        articleEntity3.setId(3L);
-        articleEntity3.setArticleId(3L);
-        articleEntity3.setStock(stockEntity3);
-        when(articleRepository.findByArticleId(articleEntity3.getId())).thenReturn(Optional.of(articleEntity3));
-
         ProductEntity productEntity = new ProductEntity();
         productEntity.setId(1L);
 
         ProductArticleEntity productArticleEntity1 = new ProductArticleEntity();
         productArticleEntity1.setId(1L);
-        productArticleEntity1.setArticleId(articleEntity1.getArticleId());
+        productArticleEntity1.setArticleId(1L);
         productArticleEntity1.setAmountOf(4L);
         productEntity.getProductArticleEntity().add(productArticleEntity1);
-
-        ProductArticleEntity productArticleEntity2 = new ProductArticleEntity();
-        productArticleEntity2.setId(1L);
-        productArticleEntity2.setArticleId(articleEntity2.getArticleId());
-        productArticleEntity2.setAmountOf(2L);
-        productEntity.getProductArticleEntity().add(productArticleEntity2);
-
-        ProductArticleEntity productArticleEntity3 = new ProductArticleEntity();
-        productArticleEntity3.setId(1L);
-        productArticleEntity3.setArticleId(articleEntity3.getArticleId());
-        productArticleEntity3.setAmountOf(3L);
-        productEntity.getProductArticleEntity().add(productArticleEntity3);
 
         List<ProductEntity> productEntityList = new ArrayList<>();
         productEntityList.add(productEntity);
